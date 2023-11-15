0ff823998e59078294bdd9534794448929472876
Robert Turnbull <rturnbull@rdl-ejx5s7.desktop.cloud.unimelb.edu.au>
2023-11-15 12:15:19 +1100

    :abc: updating date

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 
 project = "Seqbank"
-copyright = "2022, Robert Turnbull"
+copyright = "2023, Robert Turnbull"
 author = "Robert Turnbull"
 
 # The full version, including alpha/beta/rc tags
