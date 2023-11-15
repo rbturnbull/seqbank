3b8cbe2c1f98aaa885c10fcc6d7a9944da685878
Robert Turnbull <rturnbull@rdl-ejx5s7.desktop.cloud.unimelb.edu.au>
2023-11-15 12:03:34 +1100

    :cop: updating test now that ive fixed an issue with infering the format from the suffix

--- tests/test_io.py
+++ tests/test_io.py
@@ -15,7 +15,7 @@ def test_get_file_format():
     assert get_file_format("test.gbk.gz") == "genbank"        
 
 
-    assert get_file_format("test.tsv.gz") == "tsv"        
+    assert get_file_format("test.tsv.gz") == "tab"        
     
     assert get_file_format("test.fastq") == "fastq"        
     assert get_file_format("test.fq") == "fastq"        
