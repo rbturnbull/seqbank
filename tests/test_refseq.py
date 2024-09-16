41b52fbde001965e38585349d13c9aad07bb3420
Rafsan Al Mamun <64797072+Rafsan7238@users.noreply.github.com>
2024-09-16 13:34:19 +1000

    Added tests for get_refseq_urls()

--- tests/test_refseq.py
+++ tests/test_refseq.py
@@ -28,3 +28,13 @@ def test_get_refseq_filenames(mock_read_text, mock_download_file):
     # Test the get_refseq_filenames function
     filenames = get_refseq_filenames(tmp_dir=None)
     assert filenames == ['refseq.2.genomic.fna.gz', 'refseq.10.genomic.fna.gz', 'refseq.100.genomic.fna.gz']
+
+def test_get_refseq_urls(mock_download_file):
+    # Mocking the output of get_refseq_filenames
+    with patch('seqbank.refseq.get_refseq_filenames', return_value=['refseq.2.genomic.fna.gz', 'refseq.10.genomic.fna.gz']):
+        urls = get_refseq_urls(tmp_dir=None)
+        expected_urls = [
+            'https://ftp.ncbi.nlm.nih.gov/refseq/release/complete/refseq.2.genomic.fna.gz',
+            'https://ftp.ncbi.nlm.nih.gov/refseq/release/complete/refseq.10.genomic.fna.gz'
+        ]
+        assert urls == expected_urls
